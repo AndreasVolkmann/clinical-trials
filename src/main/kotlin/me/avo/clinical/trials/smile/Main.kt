3ce430e2cb9b45808cb3a9f329585060cf1662ed
avo <avo@apurebase.com>
2017-12-04 20:04:04 +0100

    smile nlp

--- src/main/kotlin/me/avo/clinical/trials/smile/Main.kt
+++ src/main/kotlin/me/avo/clinical/trials/smile/Main.kt
@@ -3,24 +3,18 @@ package me.avo.clinical.trials.smile
 import smile.classification.*
 import smile.data.*
 import smile.data.parser.*
-import smile.math.*
 import smile.math.kernel.*
-import smile.plot.*
-import java.awt.*
-import java.awt.Color.*
 
 fun main(args: Array<String>) {
 
-    val data = DelimitedTextParser().run {
+    val data = parseDelimitedText("trials-smile-vectorized.txt") {
+        setColumnNames(true)
         setResponseIndex(NominalAttribute("class"), 0)
-        parse("trials-converted.txt")
     }
 
-    val size = 3000
+    val size = 5000
 
-    val train = data.head(size / 10 * 9)
-    val test = data.tail(size / 10)
-
+    val (train, test) = data.splitTrainTest(size / 10 * 9, size / 10)
     val (x, y) = train.getXY()
     val (testX, testY) = test.getXY()
 
@@ -31,9 +25,7 @@ fun main(args: Array<String>) {
     val error = testX.indices.count { svm.predict(testX[it]) != testY[it] }
     println("USPS error rate = ${100.0 * error / testX.size}")
 
-    ScatterPlot.plot(x.map { it.slice(0..2).toDoubleArray() }.toTypedArray(), y, CharArray(5, Int::toChar), arrayOf(BLACK , BLUE, GRAY, GREEN, ORANGE))
-            .show()
-
+    //ScatterPlot.plot(x.map { it.slice(0..2).toDoubleArray() }.toTypedArray(), y, CharArray(5, Int::toChar), arrayOf(BLACK , BLUE, GRAY, GREEN, ORANGE))
 
     /*
     println("USPS one more epoch...")
@@ -52,3 +44,10 @@ fun main(args: Array<String>) {
 fun AttributeDataset.getX(): Array<out DoubleArray> = toArray(Array(size()) { DoubleArray(0) })
 fun AttributeDataset.getY(): IntArray = toArray(IntArray(size()))
 fun AttributeDataset.getXY() = getX() to getY()
+
+fun parseDelimitedText(path: String, block: DelimitedTextParser.() -> Unit): AttributeDataset
+        = DelimitedTextParser().apply(block).parse(path)
+
+fun AttributeDataset.splitTrainTest(testSize: Int, trainSize: Int): Pair<AttributeDataset, AttributeDataset> {
+    return head(testSize) to tail(trainSize)
+}
